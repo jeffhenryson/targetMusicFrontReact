fca1a136fadf3637f9c090b0bda8db9ea6f06508
jeffhenryson <102114669+jeffhenryson@users.noreply.github.com>
2022-06-22 18:36:13 -0300

    Version 1.0.2
    Adicionando alguns modulos.

--- src/componets/Tela/Home.jsx
+++ src/componets/Tela/Home.jsx
@@ -1,12 +1,44 @@
 import React from "react";
-import { Cabecalho, Titulo, ImagemLogo } from "./estiloHome";
+import {
+  Cabecalho,
+  Botao,
+  SeparadorDiv,
+  SeparadorDivDois,
+  LogoMarca,
+} from "./estiloHome";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  let navigate = useNavigate();
+
   return (
     <div>
       <header>
         <Cabecalho>
-          <Titulo>Target Music</Titulo>
+          <SeparadorDiv></SeparadorDiv>
+          <LogoMarca
+            img
+            src={require("../imagens/target-Music.png")}
+          ></LogoMarca>
+          <SeparadorDivDois></SeparadorDivDois>
+          <Botao
+            onClick={() => {
+              navigate("sobre");
+            }}
+          >
+            Sobre{" "}
+          </Botao>
+          <SeparadorDiv></SeparadorDiv>
+          <Botao>Contato</Botao>
+          <SeparadorDiv></SeparadorDiv>
+          <Botao
+            onClick={() => {
+              navigate("cadastrocliente");
+            }}
+          >
+            Admin
+          </Botao>
+          <SeparadorDiv></SeparadorDiv>
         </Cabecalho>
       </header>
       <body></body>
